cd12ed58ff3944a530606360787137cf9278ad28
niketajain123 <niketajain236@gmail.com>
2025-01-11 13:35:27 +0530

    HarryAge added

---
+++ level1/HarryAge.java
@@ -0,0 +1,11 @@
+public class HarryAge{
+	public static void main(String[]args){
+		//create a variable birthYear to indicate Harry's born year
+		int birthYear=2000;
+		//create a variable currentYear to indicate current year
+		int currentYear=2024;
+		//printing Harry's age as Harry's age 
+		//logic used => age=currentYear-birthYear
+		System.out.println("Harry's age in "+currentYear+" is "+(currentYear-birthYear));
+}
+}
