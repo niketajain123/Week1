214ec0b0936097b58ba0f1c69808363ff1a9056a
niketajain123 <niketajain236@gmail.com>
2025-01-11 13:36:18 +0530

    KilometersToMiles added

---
+++ level1/KilometersToMiles.java
@@ -0,0 +1,10 @@
+public class KilometersToMiles{
+	public static void main(String[]args){
+		//create a variable kilometer to indicate distance in kilometers
+		double kilometers=10.8;
+		//create a variable miles to convert distance from kilometers to miles
+		double miles=kilometers*1.6;
+		//print the conversion details
+		System.out.println("The distance "+kilometers+"km in miles is "+miles);
+	}
+}
