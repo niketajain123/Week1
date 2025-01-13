cfeb1fc4c326c09fbc1a014aefb42b2afd317e79
niketajain123 <niketajain236@gmail.com>
2025-01-13 12:59:29 +0530

    UnitConvertor2 added

---
+++ level2/UnitConvertor2.java
@@ -0,0 +1,19 @@
+
+public class UnitConvertor2{
+	//defining methods
+	public static double convertYardsToFeet(double yards){
+		return yards*3;
+	}
+	public static double convertFeetToYards(double feet){
+		return feet*0.333333;
+	}
+	public static double convertMetersToInches(double meters){
+		return meters*39.3701;
+	}
+	public static double convertInchesToMeters(double inches){
+		return inches*0.0254;
+	}
+	public static double convertInchesToCentiMeters(double inches){
+		return inches*2.54;
+	}
+}
